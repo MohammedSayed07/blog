fac52803d06d3ec49ff46ffe1186ea7ddad167a4
MohammedSayed07 <mohammed.s.hegazi@gmail.com>
2023-12-06 12:19:06 +0200

    feat: Implement dynamic route for category and author avoiding the N+1 problem by eager loading the data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,12 +26,14 @@ class DatabaseSeeder extends Seeder
         ])->first();
 
         $user1 = \App\Models\User::factory(1)->create([
-            'name' => 'mickey',
+            'name' => 'Mohammed Sayed',
+            'username' => 'mickey',
             'email' => 'mickey@example.com'
         ])->first();
 
         $user2 = \App\Models\User::factory(1)->create([
-            'name' => 'abdo',
+            'name' => 'Abdelrahman Yousri',
+            'username' => 'abdo',
             'email' => 'abdelrahman@example.com'
         ])->first();
 
@@ -52,7 +54,7 @@ class DatabaseSeeder extends Seeder
 
         \App\Models\Post::factory(5)->create([
             'user_id' => $user2->id,
-            'category_id' => $category2->id
+            'category_id' => $category2 ->id
         ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -9,7 +9,16 @@
         <h1>
             {{$post->title}}
         </h1>
-
+        <p>
+            By
+            <a href="#">
+                {{$post->author->name}}
+            </a>
+            in
+            <a href="/categories/{{$post->category->slug}}">
+                {{$post->category->name}}
+            </a>
+        </p>
         <p>
             {{$post->body}}
         </p>
