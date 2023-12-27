910dbe1188ffa85298fd1ec296bfae0b25d29db4
MohammedSayed07 <mohammed.s.hegazi@gmail.com>
2023-12-27 08:10:18 +0200

    feat: Implement newsletter signup functionality
    Introduce a new route for handling newsletter signups. The backend logic validates the email address, connects to the Mailchimp API, and subscribes the user to the specified mailing list. Refactoring has been performed to enhance code readability and maintainability. Exception handling ensures a smooth user experience, redirecting to the appropriate section on validation errors or exceptions. Upon successful signup, users are redirected to the home page with a success message.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -34,7 +34,7 @@
                 </form>
             @endguest
 
-            <a href="#" class="transition-all duration-300 bg-blue-500 hover:bg-blue-600 ml-3 rounded-full text-xs font-semibold text-white uppercase px-5 py-3">
+            <a href="#newsletter" class="transition-all duration-300 bg-blue-500 hover:bg-blue-600 ml-3 rounded-full text-xs font-semibold text-white uppercase px-5 py-3">
                 Subscribe for Updates
             </a>
         </div>
@@ -42,7 +42,7 @@
 
     {{$slot}}
 
-    <footer class="max-w-6xl mx-auto mt-16 bg-gray-100 border border-black border-opacity-5 rounded-xl px-12 py-12 text-center">
+    <footer id="newsletter" class="max-w-6xl mx-auto mt-16 bg-gray-100 border border-black border-opacity-5 rounded-xl px-12 py-12 text-center">
         <div>
             <img class="mx-auto rounded-xl" src="/images/head.png" width="128"/>
 
@@ -52,12 +52,15 @@
         </div>
 
         <div class="inline-block mt-6">
-            <form method="post" class="lg:flex text-sm lg:bg-gray-200 lg:rounded-full">
-                <div class="bg-gray-200 py-3 px-3 inline-flex items-center rounded-full">
-                    <label for="email">
-                        <svg fill="gray" width="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><g data-name="5-Email-Check"><path d="M29 4H3a3 3 0 0 0-3 3v18a3 3 0 0 0 3 3h13v-2H3a1 1 0 0 1-1-1V7.23l13.42 9.58a1 1 0 0 0 1.16 0L30 7.23V17h2V7a3 3 0 0 0-3-3zM16 14.77 3.72 6h24.56z"/><path d="m22 25.59-2.29-2.29-1.41 1.41 3 3a1 1 0 0 0 1.41 0l7-7-1.41-1.41z"/></g></svg>
-                    </label>
-                    <input id="email" class="bg-transparent pl-4 focus-within:outline-none" type="text" placeholder="Your e-mail address">
+            <form method="POST" action="/newsletter" class="lg:flex text-sm lg:bg-gray-200 lg:rounded-full">
+                @csrf
+                <div>
+                    <div class="bg-gray-200 py-3 px-3 inline-flex items-center rounded-full">
+                        <label for="email">
+                            <svg fill="gray" width="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><g data-name="5-Email-Check"><path d="M29 4H3a3 3 0 0 0-3 3v18a3 3 0 0 0 3 3h13v-2H3a1 1 0 0 1-1-1V7.23l13.42 9.58a1 1 0 0 0 1.16 0L30 7.23V17h2V7a3 3 0 0 0-3-3zM16 14.77 3.72 6h24.56z"/><path d="m22 25.59-2.29-2.29-1.41 1.41 3 3a1 1 0 0 0 1.41 0l7-7-1.41-1.41z"/></g></svg>
+                        </label>
+                        <input id="email" name="email" class="bg-transparent pl-4 focus-within:outline-none" type="text" placeholder="Your e-mail address">
+                    </div>
                 </div>
 
                 <div>
@@ -67,6 +70,13 @@
                 </div>
 
             </form>
+            <div>
+                <p class="text-red-500 text-xs">
+                    @error('email')
+                    {{ $message }}
+                    @enderror
+                </p>
+            </div>
         </div>
     </footer>
 
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,9 +18,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('newsletter', function () {
+    try {
+        request()->validate([
+            'email' => ['required', 'email']
+        ]);
+
+        $mailchimp = new \MailchimpMarketing\ApiClient();
+
+        $mailchimp->setConfig([
+            'apiKey' => config('services.mailchimp.key'),
+            'server' => 'us21'
+        ]);
+
+        $response = $mailchimp->lists->addListMember("83bcaf239b", [
+            'email_address' => request('email'),
+            'status' => 'subscribed'
+        ]);
+    } catch (ValidationException $e) {
+        throw $e::withMessages([
+            'email' => $e->getMessage()
+        ])->redirectTo('/#newsletter');
+    } catch (\Exception $e) {
+        throw ValidationException::withMessages([
+            'email' => 'This email could not be signed up'
+        ])->redirectTo('/#newsletter');
+    }
+
+    return redirect('/')
+        ->with('success', 'You are signed up to our newsletter');
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show'] )->name('post');
-Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store'])->middleware('auth');
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
