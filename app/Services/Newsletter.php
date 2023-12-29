cea4c6048c17e1dc09efe45cd749dfecc1add976
MohammedSayed07 <mohammed.s.hegazi@gmail.com>
2023-12-29 04:56:58 +0200

    refactor: Implement Dependency Injection for Newsletter Service
    Enhance code maintainability by refactoring the Newsletter service to utilize Dependency Injection. Introduce an interface for the Newsletter service, allowing for seamless switching of implementations. The new MailchimpNewsletter class adheres to the interface, promoting code cohesion and readability. The AppServiceProvider demonstrates skillful use of Laravel container bindings, ensuring a clean separation of concerns in the application's service layer.

--- app/Services/Newsletter.php
+++ app/Services/Newsletter.php
@@ -2,30 +2,7 @@
 
 namespace App\Services;
 
-use MailchimpMarketing\ApiClient;
-
-class Newsletter
+interface Newsletter
 {
-    public function subscribe(string $email, $list = null)
-    {
-        $list ??= config('services.mailchimp.list.subscribers');
-
-        return $this->client()->lists
-            ->addListMember($list, [
-            'email_address' => $email,
-            'status' => 'subscribed'
-        ]);
-    }
-
-    protected function client(): ApiClient
-    {
-        $mailchimp = new ApiClient();
-
-        $mailchimp->setConfig([
-            'apiKey' => config('services.mailchimp.key'),
-            'server' => 'us21'
-        ]);
-
-        return $mailchimp;
-    }
+    public function subscribe(string $email, $list = null);
 }
